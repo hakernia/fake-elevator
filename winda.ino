1667005c23001f3907f334d9bdf4d3bd19dc2950
Piotr Karpiewski <karpiewski.git@gmail.com>
2019-03-11 22:59:19 +0100

    Introduce speech (Wtv020sd16p); long key press

--- winda.ino
+++ winda.ino
@@ -21,18 +21,23 @@
 	
 	
 	This revision:
-	  - introduce virtual keys (shift key functionality) 
-	    in order to handle 16 switches with just 10 floor keys
-	  - total virtual keys:
-	    10 floor keys
-		10 floor with "shift" keys
-		P key (the ground floor) works as shift
-		STOP key
-		RING key
+	  - introduce speech (handle Wtv020sd16p module)
+	  - handle long key press
 	
 *************************************************************************/
 
 #include "FastLED.h"
+#include <Wtv020sd16p.h>
+
+// wtv020 pin
+#define WTV_RESET_PIN    9
+#define WTV_CLOCK_PIN    5 
+#define WTV_DATA_PIN     6
+#define WTV_BUSY_PIN    19  // not used if async play only
+
+// init wtv management object
+Wtv020sd16p wtv020sd16p(WTV_RESET_PIN, WTV_CLOCK_PIN, WTV_DATA_PIN, WTV_BUSY_PIN);
+
 
 // How many leds in your strip?
 #define NUM_LEDS      13
@@ -60,7 +65,9 @@
 #define POLARITY_ON_PIN    11  // L - active
 #define POLARITY_OFF_PIN   13  // L - active
 
-#define NO_MATTER  0
+#define ENERGIZE_DURATION  35  // 30 - 72ms, 35 - 84ms  -> 5 - 12ms
+
+#define NO_MATTER  0  // value that does not matter, zero is fine too
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -75,6 +82,7 @@ CRGB leds[NUM_LEDS];
 #define BLINK_TIME  500
 char blink_state = 0;
 int blink_countdown = 0;
+
 
 
 // KEYPAD related declarations
@@ -102,11 +110,14 @@ char physical_key[NUM_PHYS_KEYS];
 char key[NUM_KEYS];      // 0 - released, 1 - pressed
 char last_key[NUM_KEYS]; // last state of key; used to detect a change
 #define MAX_KEY_COUNTDOWN  3000
+#define MAX_KEY_PRESS_COUNTDOWN  900
 int key_countdown[NUM_KEYS];  // indicating time after key was pressed
+int key_press_countdown[NUM_KEYS];  // indicating time the key stays pressed
 char key_num;
 char key_pressed;
 #define SLEEP_TIME  5000
 long sleep_countdown = 0;
+
 
 
 // LIFT related declarations
@@ -134,7 +145,8 @@ char floors[NUM_KEYS];
 
 
 char ff;
-char modeset_num;
+char modeset_num = 0;
+char last_active_modeset_num = 0;
 
 void setup() {
       pinMode(SHIFT_CLOCK_PIN, OUTPUT);
@@ -190,12 +202,17 @@ void setup() {
         mode[modeset_num][key_num] = 0;
       }
       
+      for(ff=0; ff<NUM_KEYS; ff++) {
+        key_countdown[ff] = MAX_KEY_COUNTDOWN;
+      }
+      for(ff=0; ff<NUM_KEYS; ff++) {
+        key_press_countdown[ff] = MAX_KEY_PRESS_COUNTDOWN;
+      }
+
+      
       // blink all LEDs to indicate the program start
       for(ff=0; ff<NUM_LEDS; ff++) {
         leds[ff] = CRGB::White;
-      }
-      for(ff=0; ff<NUM_KEYS; ff++) {
-        key_countdown[ff] = MAX_KEY_COUNTDOWN;
       }
       FastLED.show();
       delay(1000);
@@ -203,6 +220,10 @@ void setup() {
         leds[ff] = CRGB::Black;
       }
       FastLED.show();
+
+
+      // Initializes the wtv sound module
+      wtv020sd16p.reset();
 }
 
 
@@ -326,11 +347,17 @@ void mapPhysKeyToKey(char mode, char key_p_state) {
 void countdownKbd(char from_key, char to_key) {
   char key_num;
   for(key_num=from_key; key_num<=to_key; key_num++)
-    if(key[key_num] != last_key[key_num])
+    if(key[key_num] != last_key[key_num]) {
       key_countdown[key_num] = MAX_KEY_COUNTDOWN;
-    else
+      key_press_countdown[key_num] = MAX_KEY_PRESS_COUNTDOWN;
+    }
+    else {
       if(key_countdown[key_num] > 0)
         key_countdown[key_num]--;
+      if(key[key_num])  // key is pressed
+        if(key_press_countdown[key_num] > 0)
+          key_press_countdown[key_num]--;
+    }
 }
 
 
@@ -368,42 +395,97 @@ unsigned long modeset[NUM_MODESETS][5] =
 #define MODESET_FLOOR_STOP    4
 
 // set key to next color in given modeset
-void switch_mode(char key_num, char modeset_num) {
+void next_mode(char key_num, char modeset_num) {
   if(mode[modeset_num][key_num] < modeset[modeset_num][0]-1) 
     mode[modeset_num][key_num]++;
   else 
     mode[modeset_num][key_num] = 0;
 }
-void switch_mode_while_visible(char key_num, char modeset_num) {
+void switch_mode_while_visible(char key_num, char modeset_num) {  // used by bell key
   // Switch mode only if pressed while lit.
-  if(last_key[key_num] != key[key_num]) // just pressed
+  if(last_key[key_num] != key[key_num] &&   // just pressed or released
+     key_press_countdown[key_num] > 0)      // and no long press
     if(key_countdown[key_num] > 0) { // and did not sleep at the moment
-      switch_mode(key_num, modeset_num);
-
+      next_mode(key_num, modeset_num);      
+    }
+}
+void switch_mode(char key_num, char modeset_num) {  // used by all except bell key
+  if(last_key[key_num] != key[key_num] &&    // just pressed or released
+     key_press_countdown[key_num] > 0) {     // and no long press
+      next_mode(key_num, modeset_num);
+  }
+}
+void handle_queue(char key_num, char modeset_num) {  // used by individual digit keys
+  if(last_key[key_num] != key[key_num] &&    // just pressed or released
+     key_press_countdown[key_num] > 0) {     // and no long press
       // Update queue of shift register pins events
       if(mode[modeset_num][KEY_STOP])                             // key stop is active
       if(key_num >= KEY_DIGIT_MIN && key_num <= KEY_DIGIT_MAX) {  // number keys only
-        if(mode[modeset_num][key_num])
-          add_to_queue(key_num-1, 300, 1, 1, 1); // bit_num, duration, exclusive, polarity, energize
-        else
-          add_to_queue(key_num-1, 300, 1, 0, 1);
-      }
-      
-      // Update queue with all digits ON if key STOP is pressed
-      if(key_num == KEY_STOP) {
-        for(unsigned char ff = KEY_DIGIT_MIN; ff<KEY_DIGIT_MAX; ff++) {
-          if(mode[modeset_num][ff])
-            add_to_queue(ff-1, 300, 1, mode[modeset_num][KEY_STOP], 1);
+        if(mode[modeset_num][key_num]) {
+          add_to_queue(key_num-1, ENERGIZE_DURATION, 1, 1, 1); // bit_num, duration, exclusive, polarity, energize
+          last_active_modeset_num = modeset_num;
         }
+        else
+          add_to_queue(key_num-1, ENERGIZE_DURATION, 1, 0, 1);
       }
-      
-    }
+  }
 }
-// set range of keys to next color in given modeset
-void manage_key_mode(char from_key, char to_key, char modeset_num) {
-  for(key_num=from_key; key_num<=to_key; key_num++) {
-    if(key[key_num])
-      switch_mode_while_visible(key_num, modeset_num);
+void all_digits_off() {
+  for(unsigned char ff = KEY_DIGIT_MIN; ff<=KEY_DIGIT_MAX; ff++) {
+    if(mode[last_active_modeset_num][ff])
+      add_to_queue(ff-KEY_DIGIT_MIN, ENERGIZE_DURATION, 1, 0, 1);
+  }
+  mode[last_active_modeset_num][KEY_STOP] = 0;  // turn off STOP key in last modeset
+}
+void handle_queue_bulk(char key_num, char modeset_num) {  // used by stop key
+  if(last_key[key_num] != key[key_num] &&    // just pressed or released
+     key_press_countdown[key_num] > 0) {     // and no long press
+      if(last_active_modeset_num != modeset_num)
+        all_digits_off();
+      // Update queue with all digits ON if key STOP is pressed
+      for(unsigned char ff = KEY_DIGIT_MIN; ff<=KEY_DIGIT_MAX; ff++) {
+        if(mode[modeset_num][ff])
+          add_to_queue(ff-KEY_DIGIT_MIN, ENERGIZE_DURATION, 1, mode[modeset_num][KEY_STOP], 1);
+          if(mode[modeset_num][KEY_STOP])
+            last_active_modeset_num = modeset_num;
+      }
+  }
+}
+void handle_long_press(char key_num, char modeset_num) {  // used by stop key
+  if(key_press_countdown[key_num] == 0) { // long pressed
+    key_press_countdown[key_num] = -1;    // avoid reexecution at the next tick
+    // Update all digits to ON or OFF if key STOP is pressed long
+    for(unsigned char ff = KEY_DIGIT_MIN; ff<=KEY_DIGIT_MAX; ff++) {
+      if(mode[modeset_num][KEY_STOP] != mode[modeset_num][ff]) {
+          // update to mode of the stop key
+          next_mode(ff, modeset_num);
+          add_to_queue(ff-KEY_DIGIT_MIN, ENERGIZE_DURATION, 1, mode[modeset_num][KEY_STOP], 1);
+          if(mode[modeset_num][KEY_STOP])
+            last_active_modeset_num = modeset_num;
+      }
+    }
+  }
+}
+// handle pressed keys
+void manage_key_mode(char modeset_num) {
+  if(key[KEY_BELL])
+    switch_mode_while_visible(KEY_BELL, modeset_num);
+  if(key[KEY_P]) {
+    switch_mode(KEY_P, modeset_num);
+  }
+  if(key[KEY_STOP]) {
+    //switch_mode(KEY_STOP, modeset_num);  // just pressed
+    //handle_queue_bulk(KEY_STOP, modeset_num); // just pressed
+    handle_long_press(KEY_STOP, modeset_num);
+  } else {
+    switch_mode(KEY_STOP, modeset_num);  // just released
+    handle_queue_bulk(KEY_STOP, modeset_num); // just released
+  }
+  for(key_num=KEY_DIGIT_MIN; key_num<=KEY_DIGIT_MAX; key_num++) {
+    if(key[key_num]) {
+      switch_mode(key_num, modeset_num);
+      handle_queue(key_num, modeset_num);
+    }
   }
 }
 
@@ -441,12 +523,15 @@ void display_based_on_mode(char from_led, char to_led, char modeset_num, char ke
       else
         // digit keys
         key_num = led_num + key_p_state * 10;
-        
-      if(key_countdown[key_num] > 0) {
+
+      if(key_num == KEY_BELL) {
+        if(key_countdown[key_num] > 0)
           leds[keymap[key_num]] = modeset[modeset_num][mode[modeset_num][key_num]+1];
+        else
+          leds[keymap[key_num]] = CRGB::Black;
       }
       else
-        leds[keymap[key_num]] = CRGB::Black;
+        leds[keymap[key_num]] = modeset[modeset_num][mode[modeset_num][key_num]+1];
     }
 }
 
@@ -490,6 +575,10 @@ unsigned char value[MAX_QUEUE] = {0};  // bit 2 - exclusive, bit 1 - polarity, b
 unsigned char bit_num[MAX_QUEUE];
 unsigned char queue_start = 0;
 unsigned char queue_end = queue_start;
+
+unsigned char map_key_to_sr[1][NUM_KEYS] = {{0, 
+                                            8, 7, 6, 4, 14, 12, 3, 15, 13, 11, 10, 9, 2, 5, 1,
+                                            16, 17, 18, 19, 20, 21, 22}};
 
 // output buffer for shift register
 int sr_data = 0x0000;
@@ -539,7 +628,7 @@ void process_queue_tick() {
 
         // avoid repeating sending sr data
         if(8 & value[ff]) {
-          set_sr_output_pin(bit_num[ff], 2 & value[ff], 1 & value[ff]);
+          set_sr_output_pin(map_key_to_sr[0][bit_num[ff]], 2 & value[ff], 1 & value[ff]);
           value[ff] &= 0xF7; // 11110111 - clear init bit
         }
         
@@ -550,7 +639,7 @@ void process_queue_tick() {
       }
     }
     if(duration[ff] == 0) {
-      set_sr_output_pin(bit_num[ff], 0, 0);  // deenergize
+      set_sr_output_pin(map_key_to_sr[0][bit_num[ff]], 0, 0);  // deenergize
     }
     
     ff++;
@@ -582,7 +671,7 @@ int add_to_queue(char new_bit_num, int new_duration, char exclusive, char polari
     bit_num[queue_end] = new_bit_num;
     duration[queue_end] = new_duration;
     // bit 3 (val 8) indicates initialization
-    value[queue_end] = 8 + ((exclusive > 0) << 2) + ((polarity > 0) << 1) + (energize > 0);
+    value[queue_end] = (unsigned char) (8 + ((exclusive > 0) << 2) + ((polarity > 0) << 1) + (energize > 0));
 
     queue_end++;
     if(queue_end == MAX_QUEUE)
@@ -614,10 +703,13 @@ void display_debug_2() {
       if(duration[ff] > 0)
         dd++;
     }
-    leds[keymap[dd]] = CRGB::Blue;
-    
-    leds[keymap[queue_start]] = CRGB::Green;
-    leds[keymap[queue_end]] = CRGB::Red;
+    if(dd < NUM_KEYS)
+      leds[keymap[dd]] = CRGB::Blue;
+
+    if(queue_start < NUM_KEYS)
+      leds[keymap[queue_start]] = CRGB::Green;
+    if(queue_end < NUM_KEYS)
+      leds[keymap[queue_end]] = CRGB::Red;
 
     leds[0] = CRGB::Blue;
 }
@@ -672,6 +764,7 @@ void loop() {
            }
            if(state_countdown == 0) {
              state = LIFT_STOPPED;
+             wtv020sd16p.asyncPlayVoice(1);
            }
            break;
     case LIFT_STOPPED:
@@ -681,6 +774,7 @@ void loop() {
            }
            if(state_countdown == 0) {
              state = DOOR_OPENING;
+             wtv020sd16p.asyncPlayVoice(2);
            }
            break;
     case DOOR_OPENING:
@@ -690,6 +784,7 @@ void loop() {
            }
            if(state_countdown == 0) {
              state = DOOR_OPEN;
+             wtv020sd16p.asyncPlayVoice(3);
            }
            break;
     case DOOR_OPEN:
@@ -698,8 +793,10 @@ void loop() {
              state_countdown = 500;  // for how long the door is open
            }
            if(state_countdown == 0) {
-             if(target_floor != curr_floor)
+             if(target_floor != curr_floor) {
                state = DOOR_CLOSING;
+               wtv020sd16p.asyncPlayVoice(4);
+             }
            }
            break;
     case DOOR_CLOSING:
@@ -708,8 +805,10 @@ void loop() {
              state_countdown = 500;  // how long takes closing the door
            }
            if(state_countdown == 0) {
-             if(target_floor != curr_floor)
+             if(target_floor != curr_floor) {
                state = DOOR_CLOSED;
+               wtv020sd16p.asyncPlayVoice(5);
+             }
            }
            break;
     case DOOR_CLOSED:
@@ -718,8 +817,10 @@ void loop() {
              state_countdown = 500;  // for how long the lift holds after closing the door before it starts running
            }
            if(state_countdown == 0) {
-             if(target_floor != curr_floor)
+             if(target_floor != curr_floor) {
                state = LIFT_STARTING;
+               wtv020sd16p.asyncPlayVoice(6);
+             }
            }
            break;
     case LIFT_STARTING:
@@ -728,10 +829,14 @@ void loop() {
              state_countdown = 500;  // for how long the lift decides where to go
            }
            if(state_countdown == 0) {
-             if(dir > 0)
+             if(dir > 0) {
                state = LIFT_RUNNING_UP;
-             else
+               wtv020sd16p.asyncPlayVoice(7);
+             }
+             else {
                state = LIFT_RUNNING_DOWN;
+               wtv020sd16p.asyncPlayVoice(8);
+             }
            }
            break;
     case LIFT_RUNNING_UP:
@@ -748,10 +853,12 @@ void loop() {
              if(curr_floor == target_floor) {
                state = LIFT_STOPPING;
                floors[curr_floor] = 0;
+               wtv020sd16p.asyncPlayVoice(9);
              }
              else {
                last_state = RESTART_THIS_STATE;
                state = LIFT_RUNNING_UP;
+               wtv020sd16p.asyncPlayVoice(1);
              }
            }
            break;
@@ -769,10 +876,12 @@ void loop() {
              if(curr_floor == target_floor) {
                state = LIFT_STOPPING;
                floors[curr_floor] = 0;
+               wtv020sd16p.asyncPlayVoice(11);
              }
              else {
                state = LIFT_RUNNING_DOWN;
                last_state = RESTART_THIS_STATE;
+               wtv020sd16p.asyncPlayVoice(12);
              }
            }
            break;
@@ -795,11 +904,11 @@ void loop() {
     sleep_countdown = SLEEP_TIME;
 
   
-  countdownKbd(22, 22);  // fade out just the two "function" keys 
+  countdownKbd(0, 22);  // calculate long press and fade for key range
   memcpy(last_key, key, sizeof(last_key));
   readKbd();
-  mapPhysKeyToKey(mode[0][KEY_BELL],            // key 22 - bell, it's state determines mode
-                  mode[ mode[0][KEY_BELL] ][0]);  // key 0 - P
+  mapPhysKeyToKey(mode[0][KEY_BELL],            // key 22 - bell, its state determines mode
+                  mode[ mode[0][KEY_BELL] ][0]);  // key 0 - P, its state determines digits bank
 
 /*
   if(key[11]) {
@@ -832,23 +941,23 @@ void loop() {
       // uses: curr_floor, blink_state, sleep_countdown, blink_countdown, leds[], keymap[], floors[]
 
       // handle STOP key (KEY_STOP)
-      manage_key_mode(KEY_STOP, KEY_STOP, MODESET_FLOOR_STOP);  // from key, to key, mode set
+      manage_key_mode(MODESET_FLOOR_STOP);  // from key, to key, mode set
       display_based_on_mode(LED_STOP, LED_STOP, MODESET_FLOOR_STOP, mode[MODESET_FLOOR_STOP][KEY_P]);
     }
     else
     if(mode[0][KEY_BELL] == 1) {
       // Switch/Toggle mode
-      manage_key_mode(KEY_P, KEY_STOP, MODESET_SWITCHES_1);  // from key, to key, mode set
+      manage_key_mode(MODESET_SWITCHES_1);  // from key, to key, mode set
       display_based_on_mode(LED_P, LED_STOP, MODESET_SWITCHES_1, mode[MODESET_SWITCHES_1][KEY_P]);
     }
     else
     if(mode[0][KEY_BELL] == 2) {
-      manage_key_mode(KEY_P, KEY_STOP, MODESET_SWITCHES_2);
+      manage_key_mode(MODESET_SWITCHES_2);
       display_based_on_mode(LED_P, LED_STOP, MODESET_SWITCHES_2, mode[MODESET_SWITCHES_2][KEY_P]);
     }
     else
     if(mode[0][KEY_BELL] == 3) {
-      manage_key_mode(KEY_P, KEY_STOP, MODESET_SWITCHES_3);
+      manage_key_mode(MODESET_SWITCHES_3);
       display_based_on_mode(LED_P, LED_STOP, MODESET_SWITCHES_3, mode[MODESET_SWITCHES_3][KEY_P]);
       //dim_turned_off_by_group(mode[mode[0][KEY_BELL]][11], 0, 10);
     }
